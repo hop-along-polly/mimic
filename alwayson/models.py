041cf810159497154a7f1ab10f929a37a220bb08
Derek Drummond <derek.ac.drummond@gmail.com>
2024-05-15 16:28:31 -0600

    Connect GET echo endpt to DB

--- alwayson/models.py
+++ alwayson/models.py
@@ -8,17 +8,34 @@ import jwt
 SECRET = 'always-on-secret-key'
 
 
-# TODO I Should have an AlwaysOn Request/Response Exchange
-# object where the encode and decode stuff can be abstracted
+class JWTEncodeable():
+  def encode(self) -> str:
+    # TODO I may just be able to use this as a self.__dict__ or something and it works for everything
+    # TODO I may also want this to take in a SECRET and org/tenant...actually that will be encoded
+    # by the SECRET since that will be specific to each org/tenant.
+    pass
+
+
 class AlwaysOnResponse(BaseModel):
   status_code: int # TODO could probably further restruct this to valid HTTP status codes.
   body: Union[Dict[str, Any], List[Dict]]
 
+  # TODO This is 100% a misplaced responsibility since it's decoding a list of AlwaysOnResponses
+  # and not just a single one.
+  # I think the solution is creating a Encodeable class that has default encode/decode methods
+  # and each of these classes can overwrite those as needed.
+  @classmethod
+  def decode_responses(cls, res: str):
+    decoded = jwt.decode(res, SECRET, algorithms=['HS256'])['responses']
+    return [ cls(**r) for r in decoded ]
 
-class AlwaysOnRequest(BaseModel):
+
+class AlwaysOnRequest(BaseModel, JWTEncodeable):
   method: str  # TODO make this an enum
   url: str     # TODO add some regex validation?
 
+  def encode(self):
+    return jwt.encode(self.__dict__, SECRET)
 
 # Handles the AlwaysON manifest
 class ManifestEntry(BaseModel):
